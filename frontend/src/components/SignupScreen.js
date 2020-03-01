65b94676d4c4594372b857b92407b88a4b3933ea
Fangting Chen <fc1@ualberta.ca>
2020-02-29 19:19:41 -0700

    trying to connect to backend

--- frontend/src/components/SignupScreen.js
+++ frontend/src/components/SignupScreen.js
@@ -13,12 +13,15 @@ import {
 import Icon from "react-native-vector-icons/FontAwesome5";
 import ScreenStyleSheet from "../constants/ScreenStyleSheet";
 import ValidationComponent from "react-native-form-validator";
+import { register } from "../redux/actions/authActions";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
 
 class SignupScreen extends ValidationComponent {
   constructor(props) {
     super(props);
     this.state = {
-      username: "",
+      name: "",
       email: "",
       password: "",
       confirmPassword: ""
@@ -28,9 +31,21 @@ class SignupScreen extends ValidationComponent {
 
   signup = async () => {
     console.log("singup");
+    const { name, email, password } = this.state;
+    const newUser = {
+      name,
+      email,
+      password
+    };
+    this.props.register(newUser);
     // await new Promise((resolve, reject) => {
     //   // Edit the event user clicks
-    //   this.props.signup(this.username, this.state.email, this.state.password);
+    //   const newUser = {
+    //     name,
+    //     email,
+    //     password
+    //   };
+    //   this.props.register(newUser);
     //   resolve();
     // });
   };
@@ -40,8 +55,8 @@ class SignupScreen extends ValidationComponent {
       email: { email: true, required: true }
     });
 
-    let usernameValid = this.validate({
-      username: { minlength: 3, maxlength: 30, required: true }
+    let nameValid = this.validate({
+      name: { minlength: 3, maxlength: 30, required: true }
     });
 
     let pwValid = false;
@@ -49,9 +64,9 @@ class SignupScreen extends ValidationComponent {
       pwValid = true;
     }
 
-    if (usernameValid && emailValid && pwValid) {
+    if (nameValid && emailValid && pwValid) {
       this.signup();
-    } else if (!usernameValid) {
+    } else if (!nameValid) {
       Toast.show({
         text:
           "Username should be in between 3-30 characters, please check again!"
@@ -69,6 +84,7 @@ class SignupScreen extends ValidationComponent {
         text: "Something went wrong, please check again!"
       });
     }
+    this.signup();
   }
 
   //render the screen
@@ -87,11 +103,11 @@ class SignupScreen extends ValidationComponent {
           <Item style={ScreenStyleSheet.input}>
             <Icon active name="user" style={ScreenStyleSheet.icon} />
             <Input
-              name="username"
+              name="name"
               placeholder="Username"
               placeholderColor={"grey"}
-              value={this.state.username}
-              onChangeText={text => this.setState({ username: text })}
+              value={this.state.name}
+              onChangeText={text => this.setState({ name: text })}
             />
           </Item>
           <Item style={ScreenStyleSheet.input}>
@@ -139,7 +155,6 @@ class SignupScreen extends ValidationComponent {
             style={ScreenStyleSheet.authButton}
             onPress={this.onPressSignup}
           >
-            {/* Login Button - redirect user to home screen if successfull */}
             <Text style={ScreenStyleSheet.authButtonText}> SIGNUP </Text>
           </TouchableOpacity>
         </Content>
@@ -148,7 +163,12 @@ class SignupScreen extends ValidationComponent {
   }
 }
 
-export default SignupScreen;
+SignupScreen.proptypes = {
+  register: PropTypes.func.isRequired
+};
 
-//Style Sheet
-const styles = StyleSheet.create({});
+const mapStatetoProps = state => ({
+  auth: state.auth
+});
+
+export default connect(mapStatetoProps, { register })(SignupScreen);
